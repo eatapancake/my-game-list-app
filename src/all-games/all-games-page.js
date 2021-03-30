784a0b32306b5de6f60e3212ab7f5c36e4137a42
JoshLagoons <49170964+JoshLagoons@users.noreply.github.com>
2021-03-30 02:36:01 -0500

    updated the all games
    Hopefully it runs when I get a better understanding on how to import the api

--- src/all-games/all-games-page.js
+++ src/all-games/all-games-page.js
@@ -1,7 +1,15 @@
 import React from "react";
+import showGame from "./props/games-showing";
+import "./props/all-games.css";
 
 function AllGamesPage() {
-  return <div>All Games 🎮🎱🎲🏓</div>;
-}
+  return(
+    <main>All Games 🎮🎱🎲🏓
+    <h2>The Games we present to you</h2>
+    <showGame />
+    
+    </main>
+    );
+  }
 
 export default AllGamesPage;
