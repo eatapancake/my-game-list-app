9abd6fdec02e49c85a8ad962eccb3567f465084a
Janet Santoyo <44815979+eatapancake@users.noreply.github.com>
2021-03-30 13:14:17 -0500

    Commented Out Code
    Commented out code that was breaking the app.

--- src/all-games/all-games-page.js
+++ src/all-games/all-games-page.js
@@ -3,13 +3,13 @@ import showGame from "./props/games-showing";
 import "./props/all-games.css";
 
 function AllGamesPage() {
-  return(
-    <main>All Games 🎮🎱🎲🏓
-    <h2>The Games we present to you</h2>
-    <showGame />
-    
+  return (
+    <main>
+      All Games 🎮🎱🎲🏓
+      <h2>The Games we present to you</h2>
+      {/* <showGame /> */}
     </main>
-    );
-  }
+  );
+}
 
 export default AllGamesPage;
--- src/all-games/props/games-showing.js
+++ src/all-games/props/games-showing.js
@@ -1,64 +1,63 @@
-import { useEffect, useState } from "react";
-async function fetchJson(url) {
-    
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(
-        `Something went wrong, server responded with ${response.status}`
-      );
-    }
-    
-    const json = await response.json();
-    return json;
-}
+// import { useEffect, useState } from "react";
+// async function fetchJson(url) {
 
-function showGame()
-{
-    const [gameFetch, setgameFetch] = useState({
-        isLoading: true,
-        errorMessage: "",
-    data: null,
-  });
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//       throw new Error(
+//         `Something went wrong, server responded with ${response.status}`
+//       );
+//     }
 
-  //Empty array for dependencies means the effect only runs on mount.
-  useEffect(() => {
-    async function getGame() {
-      try {
-        const params = new URLSearchParams({name});
-        if(name !== "")params.append("id",name );
-        const url = `https://www.giantbomb.com/api/games/?api_key=d908835bc2b2060f94bad64e48a217b0ee716064`;
-        //const url = `https://rawg.io/games/super-metroid`;
-        const json = await fetchJson(url);
-        const { status_code, results } = json;
-        if (status_code === 1) {
-          throw new Error("Bad API request - no results!");
-        } else if (status_code === 2) {
-          throw new Error("Bad API request - invalid parameter!");
-        }
-    
+//     const json = await response.json();
+//     return json;
+// }
 
-        //successfully passed all the error checks
-        setgameFetch({
-          isLoading: false,
-          errorMessage: "",
-          data: decodeData(results),
-        });
-      } catch (err) {
-        setgameFetch({
-          isLoading: false,
-          errorMessage:
-            "Something went wrong with the loading, please try again",
-          data: null,
-        });
-        //displays specific error for debugging in the console
-        console.error(err);
-      }
-    }
+// function showGame()
+// {
+//     const [gameFetch, setgameFetch] = useState({
+//         isLoading: true,
+//         errorMessage: "",
+//     data: null,
+//   });
 
-    getGame();
-  }, []);
-  const { isLoading, errorMessage, data } = gameFetch;
-  return [isLoading, errorMessage, data];
+//   //Empty array for dependencies means the effect only runs on mount.
+//   useEffect(() => {
+//     async function getGame() {
+//       try {
+//         const params = new URLSearchParams({name});
+//         if(name !== "")params.append("id",name );
+//         const url = `https://www.giantbomb.com/api/games/?api_key=d908835bc2b2060f94bad64e48a217b0ee716064`;
+//         //const url = `https://rawg.io/games/super-metroid`;
+//         const json = await fetchJson(url);
+//         const { status_code, results } = json;
+//         if (status_code === 1) {
+//           throw new Error("Bad API request - no results!");
+//         } else if (status_code === 2) {
+//           throw new Error("Bad API request - invalid parameter!");
+//         }
 
-}
-export default showGame;
+//         //successfully passed all the error checks
+//         setgameFetch({
+//           isLoading: false,
+//           errorMessage: "",
+//           data: decodeData(results),
+//         });
+//       } catch (err) {
+//         setgameFetch({
+//           isLoading: false,
+//           errorMessage:
+//             "Something went wrong with the loading, please try again",
+//           data: null,
+//         });
+//         //displays specific error for debugging in the console
+//         console.error(err);
+//       }
+//     }
+
+//     getGame();
+//   }, []);
+//   const { isLoading, errorMessage, data } = gameFetch;
+//   return [isLoading, errorMessage, data];
+
+// }
+// export default showGame;
