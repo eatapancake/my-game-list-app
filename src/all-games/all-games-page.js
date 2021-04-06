35d7992fa1931c7957601daea727dd0ee6a25d2f
JoshLagoons <49170964+JoshLagoons@users.noreply.github.com>
2021-04-06 03:08:15 -0500

    Added what i was working on
    Added a lot of content for the sidebar and search bar. though not sure if the api works efficiently and the text might be weird but can be fixed with the styling.

--- src/all-games/all-games-page.js
+++ src/all-games/all-games-page.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 // import showGame from "./props/games-showing";
+import { useState, useEffect } from "react";
+import mainContent from "./Components/mainContent";
+import sidebar from "./Components/sidebar";
 import "./props/all-games.css";
 import useGameData from "./use-game-data";
 import LoadingSpinner from "../common/loading-spinner";
@@ -20,6 +23,40 @@ function AllGamesPage() {
   else contents = <DisplayGames gameData={data} />;
 
   console.log(data);
+  
+  const { gameList, SetGameList } = useState([]);
+  const { topGame, SetTopGame } = useState([]);
+  const { search, SetSearch } = useState("");
+
+  const GetTopGame = async () => {
+    const temp = await fetch(
+      `https://api.igdb.com/v4/popularity/`
+    ).then((res) => res.json());
+
+    SetTopGame(temp.top.slice(0, 8));
+  };
+
+  const HandleSearch = (e) => {
+    e.preventDefault();
+
+    //console.log(search);
+    FetchGame(search);
+  }
+
+  const FetchGame = async (query) => {
+
+    const temp = await fetch(`https://api.igdb.com/v4/search/game?q=${query}&order_by=titles&sort-asc&limit=10`)
+    .then(res => res.json());
+
+    SetGameList(temp.results);
+  }
+
+  useEffect(() => {
+    GetTopGame();
+    console.log("Top Game");
+  }, []);
+  console.log(topGame);
+
 
   return (
     <main>
@@ -27,7 +64,18 @@ function AllGamesPage() {
       <h2>The Games we present to you</h2>
       {contents}
       <button onClick={onButtonClick}>{"Next -->"}</button>
-      {/* <showGame /> */}
+       <div className="all-games-page">
+        <h2>Game search with the API</h2>
+        <div className="">
+          <sidebar topGame={topGame}/>
+          <mainContent 
+          HandleSearch={HandleSearch} 
+          search={search}
+          SetSearch={SetSearch}
+          gameList={gameList}
+         />
+        </div>
+      </div>
     </main>
   );
 }
